6236272796bde3e6b031427c546df6957e855bae
kimpossible92 <78959053+kimpossible92@users.noreply.github.com>
2023-01-09 04:34:11 +0900

    ыфв
    ывыв

---
+++ CppCLR_WinformsProject2/CppCLR_WinformsProject2/Form1.h
@@ -0,0 +1,140 @@
+#pragma once
+#include "error_handler.h"
+#include "token.h"
+#include <stdlib.h>
+#include <string.h>
+#include <msclr\marshal_cppstd.h>
+//..
+using namespace msclr::interop;
+namespace CppCLR_WinformsProject2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	protected:
+	private: System::Windows::Forms::RichTextBox^ richTextBox2;
+	private: System::Windows::Forms::RichTextBox^ richTextBox3;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
+			this->richTextBox3 = (gcnew System::Windows::Forms::RichTextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(12, 12);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(208, 237);
+			this->richTextBox1->TabIndex = 1;
+			this->richTextBox1->Text = L"";
+			this->richTextBox1->SizeChanged += gcnew System::EventHandler(this, &Form1::richTextBox1_TextChanged);
+			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &Form1::richTextBox1_TextChanged);
+			// 
+			// richTextBox2
+			// 
+			this->richTextBox2->Location = System::Drawing::Point(227, 12);
+			this->richTextBox2->Name = L"richTextBox2";
+			this->richTextBox2->Size = System::Drawing::Size(224, 237);
+			this->richTextBox2->TabIndex = 2;
+			this->richTextBox2->Text = L"";
+			// 
+			// richTextBox3
+			// 
+			this->richTextBox3->Location = System::Drawing::Point(458, 13);
+			this->richTextBox3->Name = L"richTextBox3";
+			this->richTextBox3->Size = System::Drawing::Size(209, 236);
+			this->richTextBox3->TabIndex = 3;
+			this->richTextBox3->Text = L"";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(313, 258);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(157, 267);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"label1";
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(679, 293);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->richTextBox3);
+			this->Controls->Add(this->richTextBox2);
+			this->Controls->Add(this->richTextBox1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+
+		private: System::Void richTextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		 token tok(marshal_as<std::string>(this->richTextBox1->Text->ToString()));
+		 //this->richTextBox2->Text = tok.pr2();
+		 //this->richTextBox2->Text = tok.pr2();
+		 tok.print();
+		}
+	};
+}
